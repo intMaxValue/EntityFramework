82c879c882371354a64df95f2b313e6326df5100
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-10-20 15:51:00 +0300

    ADONET

---
+++ ADONET-Exercise/ADONET-Exercise/SQLQueries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONET
+{
+    public static class SQLQueries
+    {
+        public const string getVillainsWithNumberOfMinions = @"
+                      SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                        FROM Villains AS v 
+                        JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                    GROUP BY v.Id, v.Name 
+                      HAVING COUNT(mv.VillainId) > 3 
+                    ORDER BY COUNT(mv.VillainId)";
+    }
+}
---
+++ ADONET-Exercise/ADONET-Exercise/StartUp.cs
@@ -0,0 +1,33 @@
+﻿// SKYNET\SQLEXPRESS
+// ADONETMinionsDB
+
+
+using ADONET;
+using System.Data.SqlClient;
+
+internal class Program
+{
+    const string _connectionString =
+    @"Server=SKYNET\SQLEXPRESS;Database=ADONETMinionsDB;Integrated Security=True";
+    private static void Main(string[] args)
+    {
+        PrintVillainsWithMinions();
+    }
+
+    public static void PrintVillainsWithMinions()
+    {
+        using (SqlConnection sqlConn = new SqlConnection(_connectionString))
+        {
+            sqlConn.Open();
+
+            using (SqlCommand getVillainsCommand = new SqlCommand(SQLQueries.getVillainsWithNumberOfMinions, sqlConn))
+            using (SqlDataReader sqlDataReader = getVillainsCommand.ExecuteReader())
+            {
+                while (sqlDataReader.Read())
+                {
+                    Console.WriteLine($"{sqlDataReader["Name"]} - {sqlDataReader["MinionsCount"]}");
+                }
+            }
+        }
+    }
+}
