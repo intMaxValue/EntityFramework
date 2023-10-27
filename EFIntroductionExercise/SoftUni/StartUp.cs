a24064b3240a9ab54ba44b9fba506af219889e3e
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-10-27 16:37:46 +0300

    EF

---
+++ EFIntroductionExercise/SoftUni/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            
+
+        }
+
+        // Problem 3
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new 
+                {
+                    e.FirstName, e.LastName, e.MiddleName, e.JobTitle, e.Salary
+                })
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
