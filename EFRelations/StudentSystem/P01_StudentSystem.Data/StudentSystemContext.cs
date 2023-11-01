d0186613fc1bb97ebcd607b9faf56748d7cb1353
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-11-01 19:12:52 +0200

    EFRelations

---
+++ EFRelations/StudentSystem/P01_StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentSystem.P01_StudentSystem.Data.Models;
+using StudentSystem.P01_StudentSystem.Data.Models.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem.P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ResourceConfiguration());
+
+
+        }
+    }
+}
