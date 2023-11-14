f37038e6021793410a76746acc2ef82681741973
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-11-14 19:26:35 +0200

    JSON

--- JSON/CarDealer/CarDealer/StartUp.cs
+++ JSON/CarDealer/CarDealer/StartUp.cs
@@ -1,10 +1,23 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            var context = new Data.CarDealerContext();
+
+            var suppliers = File.ReadAllText("../../../Datasets/suppliers.json");
+
 
         }
+
+        // 10.ImportParts
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            return $"Successfully imported .";
+        }
+
     }
 }
