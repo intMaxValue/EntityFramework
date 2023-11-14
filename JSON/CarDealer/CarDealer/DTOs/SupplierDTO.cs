4e9f9f30c06bd4a4a82a642cb14965abb6c7be0e
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-11-14 19:37:35 +0200

    JSON

---
+++ JSON/CarDealer/CarDealer/DTOs/SupplierDTO.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace CarDealer.DTOs;
+
+public class SupplierDTO
+{
+    public string Name { get; set; }
+
+    public bool IsImporter { get; set; }
+}
--- JSON/CarDealer/CarDealer/StartUp.cs
+++ JSON/CarDealer/CarDealer/StartUp.cs
@@ -1,4 +1,8 @@
-﻿using CarDealer.Data;
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs;
+using CarDealer.Models;
+using Newtonsoft.Json;
 
 namespace CarDealer
 {
@@ -8,15 +12,27 @@ namespace CarDealer
         {
             var context = new Data.CarDealerContext();
 
+            // 10.
             var suppliers = File.ReadAllText("../../../Datasets/suppliers.json");
 
-
+            Console.WriteLine(ImportParts(context, suppliers));
         }
 
         // 10.ImportParts
         public static string ImportParts(CarDealerContext context, string inputJson)
         {
-            return $"Successfully imported .";
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            IMapper mapper = new Mapper(config);
+
+            SupplierDTO[] supplierDTOs = JsonConvert.DeserializeObject<SupplierDTO[]>(inputJson);
+
+            Supplier[] suppliers = mapper.Map<Supplier[]>(supplierDTOs);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count()}.";
         }
 
     }
