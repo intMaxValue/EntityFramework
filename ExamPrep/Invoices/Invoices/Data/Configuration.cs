a3bcc23308cc169ef6c201927c2f3c435651ea92
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-11-22 13:35:43 +0200

    Exam Prep

--- ExamPrep/Invoices/Invoices/Data/Configuration.cs
+++ ExamPrep/Invoices/Invoices/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ConnectionString = @"Server=.;Database=Invoices;Integrated Security=True;Encrypt=False";
+        public static string ConnectionString = @"Server=SKYNET\SQLEXPRESS;Database=Invoices;Integrated Security=True;Encrypt=False;TrustServerCertificate=True;Trusted_Connection=True";
     }
 }
