523296d796e59bdb896d51eb07fae9e8ac7563be
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-11-25 20:11:12 +0200

    Workshop

--- Workshop/EventMe/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EventMe.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
