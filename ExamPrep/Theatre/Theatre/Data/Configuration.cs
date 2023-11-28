dc489bd5b0bfc281058bbb950ccb6e0cf08fb7ab
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-11-28 17:26:59 +0200

    Exam Prep

---
+++ ExamPrep/Theatre/Theatre/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Theatre.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=SKYNET\SQLEXPRESS;Database=Theatre;Integrated Security=True;Encrypt=False";
+    }
+}
