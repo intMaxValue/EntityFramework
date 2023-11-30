1de110ab8ef50033eedd737bd78ca437782789ca
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-11-30 12:56:09 +0200

    Exam Prep

---
+++ ExamPrep/Boardgames/Boardgames/Data/Models/BoardgameSeller.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Boardgames.Data.Models
+{
+    public class BoardgameSeller
+    {
+        [Key]
+        [ForeignKey(nameof(Boardgame))]
+        public int BoardgameId { get; set; }
+
+        public Boardgame Boardgame { get; set; } = null!;
+
+        [Key]
+        [ForeignKey(nameof(Seller))]
+        public int SellerId { get; set; }
+
+        public Seller Seller { get; set; } = null!;
+    }
+}
