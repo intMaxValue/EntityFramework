a8819e1a32b7354104970f15a158771c10cc023d
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-01-13 12:21:23 +0200

    Create Configuration.cs

---
+++ Exam/Medicines/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace Medicines.Data
+{
+    public class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=SKYNET\SQLEXPRESS;Database=Medicines;TrustServerCertificate=True;Trusted_Connection=True;";
+    }
+}
