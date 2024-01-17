28239a191019250cfaf506577bbbdd2d56850b68
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-01-17 15:12:54 +0200

    Create Medicine.cs

---
+++ Exam/Medicines/Data/Models/Medicine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Medicines.Data.Models.Enums;
+
+namespace Medicines.Data.Models
+{
+    public class Medicine
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range(0.01, 1000.00)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+
+        [Required]
+        public DateTime ProductionDate { get; set; }
+
+        [Required]
+        public DateTime ExpiryDate { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Producer { get; set; } = null!;
+
+        [Required]
+        public int PharmacyId { get; set; }
+
+        [ForeignKey(nameof(PharmacyId))]
+        public Pharmacy Pharmacy { get; set; } = null!;
+
+        public virtual ICollection<PatientMedicine> PatientsMedicines { get; set; } = new List<PatientMedicine>();
+    }
+}
