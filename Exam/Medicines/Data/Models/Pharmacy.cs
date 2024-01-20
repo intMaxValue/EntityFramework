f60ffabccfe87f16e8e9a79ed342ec90457d2857
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-01-20 12:10:01 +0200

    Create Pharmacy.cs

---
+++ Exam/Medicines/Data/Models/Pharmacy.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Medicines.Data.Models
+{
+    public class Pharmacy
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(@"\(\d{3}\) \d{3}-\d{4}")]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        public bool IsNonStop { get; set; }
+
+        public virtual ICollection<Medicine> Medicines { get; set; } = new List<Medicine>();
+    }
+}
