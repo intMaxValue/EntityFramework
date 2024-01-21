9f4a521f16baaadae66b42e3053445d16645792b
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-01-21 11:20:27 +0200

    Create ExportPatientsXMLDto.cs

---
+++ Exam/Medicines/DataProcessor/ExportDtos/ExportPatientsXMLDto.cs
@@ -0,0 +1,22 @@
+﻿using Medicines.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Medicines.DataProcessor.ExportDtos
+{
+    [XmlType("Patient")]
+    public class ExportPatientsXMLDto
+    {
+        [XmlElement("Name")]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        public AgeGroup AgeGroup { get; set; }
+
+        [Required]
+        [XmlAttribute("Gender")]
+        public string Gender { get; set; }
+
+        public ExportMedicinesXMLDto[] Medicines { get; set; }
+    }
+}
