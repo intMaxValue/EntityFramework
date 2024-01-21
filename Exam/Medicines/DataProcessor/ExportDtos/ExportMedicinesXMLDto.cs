f21158f1a3db5c71239f94d0e9a8d61f1831b858
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-01-21 11:20:15 +0200

    Create ExportMedicinesXMLDto.cs

---
+++ Exam/Medicines/DataProcessor/ExportDtos/ExportMedicinesXMLDto.cs
@@ -0,0 +1,33 @@
+﻿using Medicines.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Medicines.DataProcessor.ExportDtos
+{
+    [XmlType("Medicine")]
+    public class ExportMedicinesXMLDto
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+
+        [XmlAttribute("Category")]
+        public string Category { get; set; }
+
+        [XmlElement("Producer")]
+        public string Producer { get; set; } = null!;
+
+        [XmlIgnore]
+        public DateTime ExpiryDate { get; set; }
+
+        [XmlElement("BestBefore")]
+        public string FormattedExpiryDate
+        {
+            get => ExpiryDate.ToString("yyyy-MM-dd");
+            set => ExpiryDate = DateTime.ParseExact(value, "yyyy-MM-dd", null);
+        }
+
+    }
+}
