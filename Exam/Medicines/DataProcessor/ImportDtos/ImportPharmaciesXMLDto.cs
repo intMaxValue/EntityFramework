69f2c7445263a75ae5eb5d5039e0214fc8a47edf
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-01-23 13:17:51 +0200

    Create ImportPharmaciesXMLDto.cs

---
+++ Exam/Medicines/DataProcessor/ImportDtos/ImportPharmaciesXMLDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Medicines.DataProcessor.ImportDtos
+{
+    [XmlType("Pharmacy")]
+    public class ImportPharmaciesXMLDto
+    {
+        [Required]
+        [XmlElement("Name")]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [XmlElement("PhoneNumber")]
+        [RegularExpression(@"\(\d{3}\) \d{3}-\d{4}")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [XmlAttribute("non-stop")]
+        public string IsNonStop { get; set; }
+
+        [XmlArray("Medicines")]
+        [XmlArrayItem("Medicine")]
+        public ImportMedicinesXMLDto[] Medicines { get; set; }
+    }
+}
